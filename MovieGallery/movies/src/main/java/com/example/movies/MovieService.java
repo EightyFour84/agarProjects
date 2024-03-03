0dd4996e978e367c7b8cb80d8756216884cb5f94
Adrian G <adriangaribaldi-@hotmail.com>
2024-03-02 20:55:35 -0600

    frontend

--- MovieGallery/movies/src/main/java/com/example/movies/MovieService.java
+++ MovieGallery/movies/src/main/java/com/example/movies/MovieService.java
@@ -2,17 +2,28 @@ package com.example.movies;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+import static com.mongodb.client.model.Filters.and;
+
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+
     public List<Movie> allMovies(){
-        return movieRepository.findAll();
+        Query query = new Query(Criteria.where(("imdbId")).ne(null));
+        return mongoTemplate.find(query, Movie.class);
     }
     public Optional<Movie> singleMovie(String imdbId){
         return movieRepository.findMovieByImdbId(imdbId);
